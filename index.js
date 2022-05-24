3245d884b822ca42651d8ca546031473b7e5b0a7
Sayed Rizwan Ahmed Shah <bukharir322@gmail.com>
2022-05-24 16:33:28 +0500

    last

--- index.js
+++ index.js
@@ -13,3 +13,4 @@ app.set('view engine', 'ejs');
 //Routes
 app.use('/', require('./routes/login'));
 const PORT = process.env.PORT || 4111;
+app.listen(PORT, console.log("Server has started at port " + PORT))
--- routes/login.js
+++ routes/login.js
@@ -4,4 +4,4 @@ const {registerView, loginView } = require('../controllers/loginController');
 const router = express.Router();
 router.get('/register', registerView);
 router.get('/login', loginView);
-module.exports = router;
+module.exports = router; 
